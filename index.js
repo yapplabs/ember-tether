c7202d9c98b0d030cd764694db20d4e2a39c77a5
Chris LoPresto <chris@chrislopresto.com>
2015-05-19 17:30:47 -0400

    Improve tether dependency management
    - Expose importBowerDependencies hook
    - Update README to explain dependency management
    - Rev to v0.1.1

--- index.js
+++ index.js
@@ -5,27 +5,15 @@ module.exports = {
   name: 'ember-tether',
 
   included: function(app) {
-    app.options = app.options || {};
-    app.options.emberTether = app.options.emberTether || {};
-    var importDependencies = app.options.emberTether.excludeDependencies !== false;
-
-    if (importDependencies) {
-      this.throwIfNestedAddon(app);
-      app.import(app.bowerDirectory + '/tether/js/utils.js');
-      app.import(app.bowerDirectory + '/tether/js/tether.js');
-      app.import(app.bowerDirectory + '/tether/js/abutment.js');
-      app.import(app.bowerDirectory + '/tether/js/constraint.js');
+    if (app.import) {
+      this.importBowerDependencies(app);
     }
   },
 
-  throwIfNestedAddon: function(app) {
-    if (!app.import) {
-      var message = 'ERROR (ember-tether): Attempting to import ' +
-        '\'ember-tether\' bower dependencies will not work when nested ' +
-        'in another addon. Please specify excludeDependencies: true in ' +
-        'the emberTether config in index.js (for addons) or Brocfile.js (for ' +
-        'apps). See instructions in the README.';
-      throw(message);
-    }
+  importBowerDependencies: function(app) {
+    app.import(app.bowerDirectory + '/tether/js/utils.js');
+    app.import(app.bowerDirectory + '/tether/js/tether.js');
+    app.import(app.bowerDirectory + '/tether/js/abutment.js');
+    app.import(app.bowerDirectory + '/tether/js/constraint.js');
   }
 };
