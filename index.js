e41d5892401cd25f12212c7c08b1d2ddbcb06110
Chris LoPresto <chris@chrislopresto.com>
2015-11-01 13:50:39 -0500

    Update to Tether 1.1.1

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var unwatchedTree  = require('broccoli-unwatched-tree');
+
 module.exports = {
   name: 'ember-tether',
 
   included: function(app) {
     if (app.import) {
-      this.importBowerDependencies(app);
+      this.importEmberTetherDependencies(app);
     }
   },
 
-  importBowerDependencies: function(app) {
-    app.import(app.bowerDirectory + '/tether/js/utils.js');
-    app.import(app.bowerDirectory + '/tether/js/tether.js');
-    app.import(app.bowerDirectory + '/tether/js/abutment.js');
-    app.import(app.bowerDirectory + '/tether/js/constraint.js');
-  }
+  importEmberTetherDependencies: function(app/*, parentAddon*/) {
+    app.import('vendor/tether/js/tether.js');
+  },
+
+  treeForVendor: function() {
+    return new Funnel(unwatchedTree(path.join(__dirname, 'node_modules/tether/dist')), {
+      srcDir: '/',
+      destDir: '/tether',
+      files: [
+        'js/tether.js'
+      ]
+    });
+  },
 };
