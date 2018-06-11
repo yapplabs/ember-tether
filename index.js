051ca3ac5a0d37d48b7a2a4ff742dc04253b2cf0
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-06-11 10:14:07 -0700

    Import tether ourselves
    ember-cli-node-assets doesn't work in a nested addon situation, instead
    we can pull Tether into our /vendor tree and include it directly.

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-tether',
+  included(app) {
+    this._super.included.apply(this, arguments)
 
-  options: {
-    nodeAssets: {
-      tether: {
-        srcDir: 'dist',
-        import: ['js/tether.js']
-      }
-    }
-  }
+    let current = this;
+    // Find our host application just like the private _findHost method does
+    do {
+      app = current.app || app;
+    } while (current.parent.parent && (current = current.parent));
+
+    app.import('vendor/tether/tether.js');
+  },
+
+  treeForVendor(vendorTree) {
+    var TetherTree = new Funnel(path.dirname(require.resolve('tether')), {
+      files: ['tether.js'],
+      destDir: '/tether',
+    });
+
+    return new MergeTrees([vendorTree, TetherTree], {
+      annotation: 'ember-tether: treeForVendor'
+    });
+  },
 };
